55dcb3cb9d9f8b42510806e52a0f3afad85cfce0
Sumon <sumonkhan1081@gmail.com>
2023-10-01 20:06:23 +0530

    introduction text animation done

--- script.js
+++ script.js
@@ -11,3 +11,26 @@ function page1(){
    })
 }
 page1();
+
+function page2(){
+   let tl = gsap.timeline({
+    scrollTrigger:{
+        trigger:'.page2',
+        // markers:true,
+        start:'top 40%',
+        end:'0% 20%',
+        scrub:1,
+    }
+   })
+
+   tl.from('.page2>h3',{
+    y:100,
+    opacity:0,
+   })
+   .from('.page2>p',{
+    opacity:0,
+    y:100,
+   })
+
+}
+page2();
